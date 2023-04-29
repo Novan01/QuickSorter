822e0642ea3cffa593f31ccb287d156c2bdec3f6
Novan01 <patrick@casaanderson.net>
2023-04-29 18:56:52 -0500

    modified:   Main.java 	modified:   QuickSorter.java

--- Main.java
+++ Main.java
@@ -1,5 +1,79 @@
+import java.util.*;
+import java.io.*;
+import java.time.*;
+
 public class Main {
     public static void main(String[] args) {
+        if(args.length != 4) {
+            System.out.println("Invalid number of arguments");
+            System.out.println("Usage: Java FileHandler [input_file_1] [input_file_2] [output_file_1] [output_file_2]");
+        }
 
+        //Parse command line arguments and create the files
+        int arraySize = Integer.parseInt(args[0]);
+        String reportFilename = args[1];
+        String unsortedFilename = args[2];
+        String sortedFilename = args[3];
+
+        //generate a random list of size arraySize
+        ArrayList<Integer> list = QuickSorter.randomListGenerator(arraySize);
+
+        //save unsorted list to file
+        try {
+            saveArrayToFile(list, unsortedFilename);
+        }
+        catch(Exception e) {
+            System.out.println("Unable to create file");
+        }
+
+        //create copies of the list to past into each quicksort function
+        ArrayList<Integer> firstSortList = new ArrayList<Integer>(list);
+        ArrayList<Integer> randomSortList = new ArrayList<Integer>(list);
+        ArrayList<Integer> threeRandomSortList = new ArrayList<Integer>(list);
+        ArrayList<Integer> threeMedianSortList = new ArrayList<Integer>(list);
+
+        //create the time variables
+        Duration first_pivot_time, random_pivot_time, median_three_random_pivot_time, median_three_pivot_time;
+        
+        //Apply sort functions on each
+        first_pivot_time = QuickSorter.timedQuickSort(firstSortList, QuickSorter.PivotStrategy.FIRST_ELEMENT);
+        random_pivot_time = QuickSorter.timedQuickSort(randomSortList, QuickSorter.PivotStrategy.RANDOM_ELEMENT);
+        median_three_random_pivot_time = QuickSorter.timedQuickSort(threeRandomSortList, QuickSorter.PivotStrategy.MEDIAN_OF_THREE_RANDOM_ELEMENTS);
+        median_three_pivot_time = QuickSorter.timedQuickSort(threeMedianSortList, QuickSorter.PivotStrategy.MEDIAN_OF_THREE_ELEMENTS);
+
+        //save sorted list to sorted file 
+        try {
+            saveArrayToFile(firstSortList, sortedFilename);
+        }
+        catch(Exception e) {
+            System.out.println("Unable to create file");
+        }
+
+        //save report to report file
+        try(PrintWriter writer = new PrintWriter(new File(reportFilename))) {
+            writer.printf("Array Size = %d\n", arraySize);
+            writer.printf("FIRST_ELEMENT : %s\n", first_pivot_time);
+            writer.printf("RANDOM_ELEMENT : %s\n", random_pivot_time);
+            writer.printf("MEDIAN_OF_THREE_RANDOM_ELEMENTS : %s\n", median_three_random_pivot_time);
+            writer.printf("MEDIAN_OF_THREE_ELEMENTS : %s\n", median_three_pivot_time);
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Unable to create file" + reportFilename);
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Sorting completed successfully");
     }
+
+    private static void saveArrayToFile(ArrayList<Integer> list, String filename) throws FileNotFoundException {
+        try(PrintWriter writer = new PrintWriter(new File(filename))) {
+            for(int i = 0; i < list.size(); i++) {
+                writer.println(list.get(i));
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Error: Failed to save array to file" + filename + " ");
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
--- QuickSorter.java
+++ QuickSorter.java
@@ -1,7 +1,4 @@
 import java.util.*;
-
-import javax.swing.text.AbstractDocument.BranchElement;
-
 import java.lang.Comparable;
 import java.lang.System;
 import java.time.Duration;
@@ -13,7 +10,7 @@ public class QuickSorter {
     }
 
     public enum PivotStrategy {
-        FIRST_ELEMENT, RANDOM_ELEMENT, MEDIAN_OF_TWO_ELEMENTS, MEDIAN_OF_THREE_ELEMENTS
+        FIRST_ELEMENT, RANDOM_ELEMENT, MEDIAN_OF_THREE_RANDOM_ELEMENTS, MEDIAN_OF_THREE_ELEMENTS
     }
 
     public static <E extends Comparable<E>> Duration timedQuickSort(ArrayList<E> list, PivotStrategy pivotStrat)throws NullPointerException {
@@ -25,19 +22,21 @@ public class QuickSorter {
         }
         Long timer = System.nanoTime();
         PivotStrategy pivot = pivotStrat;
-        switch(pivotStrat) {
+        switch(pivot) {
             case FIRST_ELEMENT:
                 firstElementQuickSort(list, 0, list.size()-1);
                 break;
             case RANDOM_ELEMENT:
                 randomQuickSort(list, 0, list.size()-1);
                 break;
-            case MEDIAN_OF_TWO_ELEMENTS:
-                medianOfTwoElementsQuickSort(list, 0, list.size()-1);
+            case MEDIAN_OF_THREE_RANDOM_ELEMENTS:
+                medianOfThreeRandomElementsQuickSort(list, 0, list.size()-1);
                 break;
             case MEDIAN_OF_THREE_ELEMENTS:
                 medianOfThreeElementsQuickSort(list, 0, list.size()-1);
                 break;
+            default:
+                standardQuickSort(list, 0, list.size()-1);
         }
         Long endTimer = System.nanoTime();
         Duration time = Duration.ofNanos(endTimer - timer);
@@ -46,35 +45,114 @@ public class QuickSorter {
 
     //if our pivot strategy is FIRST_ELEMENT
     public static <E extends Comparable<E>> void firstElementQuickSort(ArrayList<E> list, int low, int high) throws IllegalArgumentException {
-
+        if(low >= high) {
+            return;
+        }
+        else {
+            swap(list, low, high);
+            int newPivot = standardQuickSort(list, low, high);
+            firstElementQuickSort(list, low, newPivot - 1);
+            firstElementQuickSort(list, newPivot + 1, high);
+        }
     }
 
     //if our pivot strategy is RANDOM_ELEMENT
     public static <E extends Comparable<E>> void randomQuickSort(ArrayList<E> list, int low, int high) {
-
+        if(low >= high) {
+            return;
+        }
+        else {
+            int randomPivot = randomNumGenerator(low, high);
+            swap(list, randomPivot, high);
+            int newPivot = standardQuickSort(list, low, high);
+            firstElementQuickSort(list, low, newPivot - 1);
+            firstElementQuickSort(list, newPivot + 1, high);
+        }
     }
 
     //if our pivot strategy is MEDIAN_OF_TWO_ELEMENTS
-    public static <E extends Comparable<E>> void medianOfTwoElementsQuickSort(ArrayList<E> list, int low, int high) {
-
+    public static <E extends Comparable<E>> void medianOfThreeRandomElementsQuickSort(ArrayList<E> list, int low, int high) {
+        int randomOne = randomNumGenerator(low, high);
+        int randomTwo = randomNumGenerator(low, high);
+        int randomThree = randomNumGenerator(low, high);
+        if(low >= high) {
+            return;
+        }
+        else {
+            int medianPivot = findMedian(list, randomOne, randomTwo, randomThree);
+            if(medianPivot != high) {
+                swap(list, medianPivot, high);
+            }
+            int newPivot = standardQuickSort(list, low, high);
+            medianOfThreeRandomElementsQuickSort(list, low, newPivot - 1);
+            medianOfThreeRandomElementsQuickSort(list, newPivot + 1, high);
+        }
     }
 
     //if our pivot strategy is MEDIAN_OF_THREE_ELEMENTS
     public static <E extends Comparable<E>> void medianOfThreeElementsQuickSort(ArrayList<E> list, int low, int high) {
-
+        if(low >= high) {
+            return;
+        }
+        else {
+            int medianPivot = findMedian(list, low, high, (low + high)/2);
+            if(medianPivot != high) {
+                swap(list, medianPivot, high);
+            }
+            int newPivot = standardQuickSort(list, low, high);
+            medianOfThreeElementsQuickSort(list, low, newPivot - 1);
+            medianOfThreeElementsQuickSort(list, newPivot + 1, high);
+        }
     }
 
     public static <E extends Comparable<E>> int standardQuickSort(ArrayList<E> list, int low, int high) {
-        return -1;
+        E pivot = list.get(high);
+        int i = low - 1;
+        for(int x = low; x < high; x++) {
+            if(list.get(x).compareTo(pivot) <= 0) {
+                i++;
+                swap(list, i, x);
+            }
+        }
+        swap(list, i + 1, high);
+        return i + 1;
     }
 
     //helper methods
     public static int randomNumGenerator(int min, int max) throws IllegalArgumentException {
-        return -1;
+        if(min > max || (max - min + 1 > Integer.MAX_VALUE)) {
+            throw new IllegalArgumentException("The range is not valid");
+        }
+
+        return new Random().nextInt(max - min + 1) + min;
+    }
+
+    public static ArrayList<Integer> randomListGenerator(int size) {
+        ArrayList<Integer> list = new ArrayList<Integer>(size);
+        Random random = new Random();
+        for(int i = 0; i < size; i++) {
+            int rand = random.nextInt();
+            list.add(rand);
+        }
+        return list;
     }
 
     private static <E extends Comparable<E>> int findMedian(ArrayList<E> list, int x, int y, int z) {
-        return -1;
+        if ((x < y && y < z) || (z < y && y < x)) {
+            return y;
+        }
+        else if((y < x && x < z) || (z < x && x < y)) {
+            return x;
+        }
+        else {
+            return z;
+        }
+    }
+
+    public static <E extends Comparable<E>> void swap(ArrayList<E> list, int i, int j) {
+        E temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
     }
 
 
